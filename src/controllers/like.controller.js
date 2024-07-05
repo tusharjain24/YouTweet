acef303e4f0db64e6fc8e78c9db3a16ae9096739
TusharJain2000 <tusharjain01234@gmail.com>
2024-07-05 18:35:46 +0530

    Completed like.controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,20 +7,128 @@ import { asyncHandler } from "../utils/asyncHandler.util.js";
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: toggle like on video
+  if (!videoId) {
+    throw new ApiError(400, "Bad Request: Video ID is required");
+  }
+
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(400, "Unauthorized Request");
+  }
+
+  const existingVideoLike = await Like.findOne({
+    video: videoId,
+    likedBy: userId,
+  });
+  let message;
+  let actiontaken;
+  if (!existingVideoLike) {
+    actiontaken = await Like.create({
+      video: videoId,
+      likedBy: userId,
+    });
+    message = "Video has been liked by the user";
+  } else {
+    actiontaken = await Like.deleteOne(existingVideoLike._id);
+    message = "Video has been unliked by the user";
+  }
+
+  res.status(200).json(new ApiResponse(200, actiontaken, message));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-  const { commentId } = req.params;
   //TODO: toggle like on comment
+  const { commentId } = req.params;
+  if (!commentId) {
+    throw new ApiError(400, "Bad Request: Comment ID is required");
+  }
+
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(400, "Unauthorized Request");
+  }
+
+  const existingCommentLike = await Like.findOne({
+    comment: commentId,
+    likedBy: userId,
+  });
+
+  let message;
+  let actiontaken;
+
+  if (!existingCommentLike) {
+    actiontaken = await Like.create({
+      comment: commentId,
+      likedBy: userId,
+    });
+    message = "Comment has been liked by the user";
+  } else {
+    actiontaken = await Like.deleteOne(existingCommentLike._id);
+    message = "Comment has been unliked by the user";
+  }
+
+  res.status(200).json(new ApiResponse(200, actiontaken, message));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-  const { tweetId } = req.params;
   //TODO: toggle like on tweet
+  const { tweetId } = req.params;
+  if (!tweetId) {
+    throw new ApiError(400, "Bad Request: Comment ID is required");
+  }
+
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(400, "Unauthorized Request");
+  }
+
+  const existingCommentLike = await Like.findOne({
+    tweet: tweetId,
+    likedBy: userId,
+  });
+
+  let message;
+  let actiontaken;
+
+  if (!existingCommentLike) {
+    actiontaken = await Like.create({
+      tweet: tweetId,
+      likedBy: userId,
+    });
+    message = "Tweet has been liked by the user";
+  } else {
+    actiontaken = await Like.deleteOne(existingCommentLike._id);
+    message = "Tweet has been unliked by the user";
+  }
+
+  res.status(200).json(new ApiResponse(200, actiontaken, message));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
   //TODO: get all liked videos
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new ApiError(400, "Bad Request: User is required to Log In");
+  }
+
+  const likedVideos = await Like.find({
+    likedBy: userId,
+    video: { $exists: true },
+  }).select("-createdAt -updatedAt -v");
+
+  if (!likedVideos) {
+    throw new ApiError(
+      400,
+      "Something went wrong or User has not liked any videos"
+    );
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, likedVideos, "Videos liked by user has been fetched")
+    );
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -10,9 +10,9 @@ import { verifyJwt } from "../middleware/auth.middleware.js";
 const router = Router();
 router.use(verifyJwt); // Apply verifyJwt middleware to all routes in this file
 
-router.route("/toggle/v/:videoId").post(toggleVideoLike);
-router.route("/toggle/c/:commentId").post(toggleCommentLike);
-router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/toggle/video/:videoId").post(toggleVideoLike);
+router.route("/toggle/channel/:commentId").post(toggleCommentLike);
+router.route("/toggle/tweet/:tweetId").post(toggleTweetLike);
 router.route("/videos").get(getLikedVideos);
 
 export default router;
